5e7b10fdedb543e16e615e5e164ab9545ef946c3
bavitra-sruthi <bavitrasruthi45@gmail.com>
2020-04-09 10:29:25 +0530

    String

---
+++ src/com/tgt/igniteplus/ImmutableStg.java
@@ -0,0 +1,9 @@
+package com.tgt.igniteplus;
+
+public class ImmutableStg {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        s1.concat(" world");
+        System.out.println(s1);
+    }
+}
